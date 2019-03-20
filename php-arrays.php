a740fa37f5db3e5e02256feb6130bfd38303aed6
sergio-20 <nubesgladius20@gmail.com>
2019-03-19 18:33:09 -0700

    Associative Arrays, Foreach Loop

---
+++ php-arrays.php
@@ -0,0 +1,26 @@
+<?php
+
+$cars = array("Toyota", "Honda", "Volkswagen");
+
+$cars[0] = "Chevy";
+
+$fullNames = array("John" => "Smith", "John" => "Doe", "Jane" => "Doe");
+
+foreach($cars as $carBrand)
+{
+	echo $carBrand . '<br>';
+}
+
+foreach($fullNames as $key => $value)
+{
+	echo "My name is " . $key . " " . "$value";
+	echo "<br>";
+}	
+
+print_r($cars);
+
+$cars = array(0 => "Mitsubishi");
+
+echo '<br>';
+
+print_r($cars);
